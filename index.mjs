b428b8b4c299c1af2dd65ccbd6ad472f24b28281
Mert Salik <53333294+codermert@users.noreply.github.com>
2023-11-06 23:31:25 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,105 +1,64 @@
-import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 import fs from 'fs/promises';
+import cloudscraper from 'cloudscraper';
 
-const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+const userAgent =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
 
 async function kaynaktanListeye(kaynakKod) {
-    const $ = cheerio.load(kaynakKod);
-    const resimler = [];
-    $('div.view-area img').each((index, element) => {
-        resimler.push($(element).attr('src'));
-    });
-    return resimler;
+  const $ = cheerio.load(kaynakKod);
+  const resimler = [];
+  $('div.view-area img').each((index, element) => {
+    resimler.push($(element).attr('src'));
+  });
+  return resimler;
 }
 
 async function a101Brosurler() {
-    const domain = "https://www.a101.com.tr";
-    const brosurler = {};
+  const domain = 'https://www.a101.com.tr';
+  const brosurler = {};
 
-    try {
-        const yanitHafta = await fetch(`${domain}/aldin-aldin-bu-hafta-brosuru`, {
-            headers: {
-                'User-Agent': userAgent,
-            },
+  try {
+    const urls = [
+      `${domain}/aldin-aldin-bu-hafta-brosuru`,
+      `${domain}/aldin-aldin-gelecek-hafta-brosuru`,
+      `${domain}/afisler-haftanin-yildizlari`,
+      `${domain}/buyuk-oldugu-icin-ucuz-afisler`,
+      `${domain}/afisler-hadi`,
+    ];
+
+    for (const url of urls) {
+      try {
+        const response = await cloudscraper.get(url, {
+          headers: {
+            'User-Agent': userAgent,
+          },
         });
 
-        if (yanitHafta.status !== 200) {
-            console.log(await yanitHafta.text());
-            brosurler["Bu Hafta"] = [];
+        if (response.statusCode !== 200) {
+          console.log(await response.body);
+          brosurler[url] = [];
         } else {
-            const kaynakKodHafta = await yanitHafta.text();
-            brosurler["Bu Hafta"] = await kaynaktanListeye(kaynakKodHafta);
+          const kaynakKod = response.body;
+          brosurler[url] = await kaynaktanListeye(kaynakKod);
         }
-
-        const yanitGelecekHafta = await fetch(`${domain}/aldin-aldin-gelecek-hafta-brosuru`, {
-            headers: {
-                'User-Agent': userAgent,
-            },
-        });
-
-        if (yanitGelecekHafta.status !== 200) {
-            console.log(await yanitGelecekHafta.text());
-            brosurler["Gelecek Hafta"] = [];
-        } else {
-            const kaynakKodGelecekHafta = await yanitGelecekHafta.text();
-            brosurler["Gelecek Hafta"] = await kaynaktanListeye(kaynakKodGelecekHafta);
-        }
-
-        const yanitYildizlari = await fetch(`${domain}/afisler-haftanin-yildizlari`, {
-            headers: {
-                'User-Agent': userAgent,
-            },
-        });
-
-        if (yanitYildizlari.status !== 200) {
-            console.log(await yanitYildizlari.text());
-            brosurler["Haftanın Yıldızları"] = [];
-        } else {
-            const kaynakKodYildizlari = await yanitYildizlari.text();
-            brosurler["Haftanın Yıldızları"] = await kaynaktanListeye(kaynakKodYildizlari);
-        }
-
-        const yanitUcuzAfisler = await fetch(`${domain}/buyuk-oldugu-icin-ucuz-afisler`, {
-            headers: {
-                'User-Agent': userAgent,
-            },
-        });
-
-        if (yanitUcuzAfisler.status !== 200) {
-            console.log(await yanitUcuzAfisler.text());
-            brosurler["Büyük olduğu için UCUZ"] = [];
-        } else {
-            const kaynakKodUcuzAfisler = await yanitUcuzAfisler.text();
-            brosurler["Büyük olduğu için UCUZ"] = await kaynaktanListeye(kaynakKodUcuzAfisler);
-        }
-
-        const yanitHadi = await fetch(`${domain}/afisler-hadi`, {
-            headers: {
-                'User-Agent': userAgent,
-            },
-        });
-
-        if (yanitHadi.status !== 200) {
-            console.log(await yanitHadi.text());
-            brosurler["Hadi"] = [];
-        } else {
-            const kaynakKodHadi = await yanitHadi.text();
-            brosurler["Hadi"] = await kaynaktanListeye(kaynakKodHadi);
-        }
-
-        if (!Object.values(brosurler).some(list => list.length > 0)) {
-            throw new Error("Bir şeyler ters gitti.");
-        }
-
-        await fs.writeFile("A101.json", JSON.stringify(brosurler, null, 2), 'utf-8');
-
-        return brosurler;
-    } catch (error) {
+      } catch (error) {
         console.error(error);
+      }
     }
+
+    if (!Object.values(brosurler).some((list) => list.length > 0)) {
+      throw new Error('Bir şeyler ters gitti.');
+    }
+
+    await fs.writeFile('A101.json', JSON.stringify(brosurler, null, 2), 'utf-8');
+
+    return brosurler;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 (async () => {
-    await a101Brosurler();
+  await a101Brosurler();
 })();
